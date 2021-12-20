53b08a5640afc4e64dd5053e1529515e0d3ea21a
Kaleab Solomon <87313721+baby2733@users.noreply.github.com>
2021-12-20 19:43:33 +0300

    Delete website directory

--- website/src/index.js
+++
@@ -1,7 +0,0 @@
-const humburger = document.getElementById('humburger');
-const navList = document.getElementById('nav-list');
-
-
-humburger.addEventListener('click', function(){navList.classList.toggle('show');});
-
-
