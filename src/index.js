1c9e35e97489a941d89ed677900d4496b04ab8fe
Kaleab Solomon <87313721+baby2733@users.noreply.github.com>
2021-12-20 19:42:23 +0300

    Add files via upload
    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const humburger = document.getElementById('humburger');
+const navList = document.getElementById('nav-list');
+
+
+humburger.addEventListener('click', function(){navList.classList.toggle('show');});
+
+
